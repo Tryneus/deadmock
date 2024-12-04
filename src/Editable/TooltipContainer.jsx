873fc06d45d50fe661a9567e584a62cd4d4854bf
grey <marc.hesse@gmail.com>
2024-12-03 21:37:17 -0800

    make tooltip rendering a little less obtuse, no longer install window click listener unless tooltip is open

--- src/Editable/TooltipContainer.jsx
+++ src/Editable/TooltipContainer.jsx
@@ -1,67 +1,77 @@
 import classNames from 'classnames';
-import {observer} from 'mobx-react-lite';
 import {useCallback, useEffect, useRef, useState} from 'preact/hooks';
 import PropTypes from 'prop-types';
 
 import './TooltipContainer.css';
 
-const TooltipContainer = observer(({renderTooltip, direction, click, children}) => {
+const Tooltip = ({down, click, children, onClose}) => {
   const ref = useRef(null);
-  const [tooltip, setTooltip] = useState(null);
-  const down = direction === 'down';
-
-  const tooltipClasses = classNames('mock-tooltip', {
+  const classes = classNames('mock-tooltip', {
     'mock-tooltip-up':   !down,
     'mock-tooltip-down': down,
   });
 
-  const stopBubble = useCallback((e) => e.stopPropagation());
-
-  const cbProps = {};
   if (click) {
-    cbProps.onClick = useCallback((e) => {
-      if (tooltip) {
-        setTooltip(null);
-      } else {
-        setTooltip(renderTooltip());
-      }
-    }, [tooltip, renderTooltip]);
-
     useEffect(() => {
       const handleClick = (e) => {
-        if (tooltip && ref.current && !ref.current.contains(e.target)) {
-          setTooltip(null);
+        if (ref.current && !ref.current.contains(e.target)) {
+          onClose();
         }
       };
 
       window.addEventListener('click', handleClick);
       return () => window.removeEventListener('click', handleClick);
-    }, [tooltip, setTooltip, ref]);
-  } else {
-    cbProps.onMouseEnter = useCallback(() => {
-      setTooltip(renderTooltip());
-    }, [renderTooltip, setTooltip]);
-
-    cbProps.onMouseLeave = useCallback(() => {
-      setTooltip(null);
-    }, [setTooltip]);
+    }, [ref, onClose]);
   }
 
-  const renderedTooltip = tooltip ? (
-    <div className={tooltipClasses} onClick={stopBubble}>
-      {tooltip}
-    </div>
-  ) : null;
-
-
   return (
-    <div ref={ref} className="mock-tooltip-container" {...cbProps} >
-      {down && children}
-      {renderedTooltip}
-      {!down && children}
+    <div ref={ref} className={classes}>
+      {children}
     </div>
   );
-});
+};
+
+Tooltip.propTypes = {
+  click: PropTypes.bool,
+  down:  PropTypes.bool,
+};
+
+const TooltipContainer = ({renderTooltip, direction, click, children}) => {
+  const ref = useRef(null);
+  const [open, setOpen] = useState(false);
+  const down = direction === 'down';
+
+  const onToggle = useCallback(() => {
+    setOpen((x) => !x);
+  }, [setOpen]);
+  const onOpen = useCallback(() => {
+    setOpen(true);
+  }, [setOpen]);
+  const onClose = useCallback(() => {
+    setOpen(false);
+  }, [setOpen]);
+
+  const cbProps = {};
+  if (click) {
+    cbProps.onClick = onToggle;
+  } else {
+    cbProps.onMouseEnter = onOpen;
+    cbProps.onMouseLeave = onClose;
+  }
+
+  const tooltip = !open ? null : (
+    <Tooltip down={down} click={click} onClose={onClose}>
+      {renderTooltip()}
+    </Tooltip>
+  );
+
+  return (
+    <div className="mock-tooltip-container">
+      <div {...cbProps}>{children}</div>
+      {tooltip}
+    </div>
+  );
+};
 
 TooltipContainer.propTypes = {
   renderTooltip: PropTypes.func.isRequired,
