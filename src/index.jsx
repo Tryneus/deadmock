9ff837da72d0323bcb1f668ff3dd90b63860959f
grey <marc.hesse@gmail.com>
2024-12-09 13:43:35 -0800

    fix fragment loading to work if fragment is modified without a page load

--- src/index.jsx
+++ src/index.jsx
@@ -5,13 +5,19 @@ import {State} from './State';
 import './style.css';
 
 const state = new State();
-if (window.location.hash) {
-  try {
-    state.deserializeActive(JSON.parse(window.atob(window.location.hash.replace('#', ''))));
-    window.location.hash = '';
-  } catch (e) {
-    console.error('failed to load model from fragment:', e);
+
+const loadFromFragment = () => {
+  if (window.location.hash !== '' && window.location.hash !== '#') {
+    try {
+      state.deserializeActive(JSON.parse(window.atob(window.location.hash.replace('#', ''))));
+      window.location.hash = '';
+    } catch (e) {
+      console.error('failed to load model from fragment:', e);
+    }
   }
-}
+};
+
+window.addEventListener('hashchange', loadFromFragment);
+loadFromFragment();
 
 render(<Editor state={state} />, document.getElementById('app'));
