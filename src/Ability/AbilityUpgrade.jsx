65b0f03dba988b8663b600d28c41d1dd04e3c72e
grey <marc.hesse@gmail.com>
2024-12-22 13:22:22 -0800

    fix ability upgrade styling when bolded text is not in a simple 'value' upgrade

--- src/Ability/AbilityUpgrade.jsx
+++ src/Ability/AbilityUpgrade.jsx
@@ -7,7 +7,15 @@ import {Bold, EditableMarkdown} from '/src/Text';
 
 const tierCosts = [1, 2, 5];
 
+const valueRegex = /^[+-]?\*?\*?[+-]?[0-9.]+[A-z/%]+\*?\*?\n[A-z ]+$/;
+
 const upgradeMarkdownFormat = {
+  text:     {},
+  strong:   {weight: 700},
+  emphasis: {weight: 500, color: 'muted', italic: true},
+};
+
+const upgradeValueMarkdownFormat = {
   text:     {},
   strong:   {weight: 700, color: 'bright'},
   emphasis: {weight: 500, color: 'muted', italic: true},
@@ -20,14 +28,19 @@ const AbilityUpgrade = observer(({active, model, tier}) => {
 
   const onChange = useAction((x) => (model.upgrades[tier] = x), [model, tier]);
 
+  // If the markdown is just a value, the styling is slightly different
+  const isValue = Boolean(model.upgrades[tier].match(valueRegex));
+  const markdownFormat = isValue ? upgradeValueMarkdownFormat : upgradeMarkdownFormat;
+  const textClasses = classNames('mock-ability-upgrade-text', {'mock-ability-upgrade-value': isValue});
+
   return (
     <div className={classes}>
       <div className="mock-ability-upgrade-cost">
         <Icon color="purple" image="stat/ability_point" />
         <Bold>{tierCosts[tier]}</Bold>
       </div>
-      <div className="mock-ability-upgrade-text">
-        <EditableMarkdown format={upgradeMarkdownFormat} text={model.upgrades[tier]} onChange={onChange} />
+      <div className={textClasses}>
+        <EditableMarkdown format={markdownFormat} text={model.upgrades[tier]} onChange={onChange} />
       </div>
     </div>
   );
