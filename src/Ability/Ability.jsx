34a5356b5b398b90e713e84942a40da299f556b0
grey <marc.hesse@gmail.com>
2026-01-31 19:55:59 -0800

    checkpoint, probably scrapping the card transform

--- src/Ability/Ability.jsx
+++ src/Ability/Ability.jsx
@@ -10,21 +10,23 @@ import './Ability.css';
 const Ability = observer(({model}) => {
   return (
     <div className="mock-ability">
-      <div className="mock-ability-body-background">
-        <div className="mock-ability-body-noise" />
-        <AnimatedDiv className="mock-ability-header-animated">
-          <AbilityHeader model={model} />
+      <div className="mock-ability-card">
+        <div className="mock-ability-card-shadow" />
+        <div className="mock-ability-card-background" />
+        <div className="mock-ability-card-noise" />
+      </div>
+      <AnimatedDiv className="mock-ability-header-animated">
+        <AbilityHeader model={model} />
+      </AnimatedDiv>
+      <div className="mock-ability-body">
+        <AnimatedDiv>
+          <Details model={model.details} />
         </AnimatedDiv>
-        <div className="mock-ability-body">
-          <AnimatedDiv>
-            <Details model={model.details} />
-          </AnimatedDiv>
-          <AnimatedDiv className="mock-ability-upgrades-animated">
-            <div className="mock-ability-upgrades">
-              {model.upgrades.map((x, i) => <AbilityUpgrade key={i} model={model} tier={i} />)}
-            </div>
-          </AnimatedDiv>
-        </div>
+        <AnimatedDiv className="mock-ability-upgrades-animated">
+          <div className="mock-ability-upgrades">
+            {model.upgrades.map((x, i) => <AbilityUpgrade key={i} model={model} tier={i} />)}
+          </div>
+        </AnimatedDiv>
       </div>
     </div>
   );
